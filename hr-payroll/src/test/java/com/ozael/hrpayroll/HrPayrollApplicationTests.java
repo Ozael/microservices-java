31e4cac70333ed33147f657c25c30da37df2b556
Ozael <ozaelvf@gmail.com>
2024-01-23 16:32:45 -0300

    hr-payroll created

---
+++ hr-payroll/src/test/java/com/ozael/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozael.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
