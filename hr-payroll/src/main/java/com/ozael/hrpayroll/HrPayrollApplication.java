d13ebcc0244e7ffed278b1904ee5191a968972a6
Ozael <ozaelvf@gmail.com>
2024-01-24 12:01:11 -0300

    Feign Client

--- hr-payroll/src/main/java/com/ozael/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/ozael/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.ozael.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
